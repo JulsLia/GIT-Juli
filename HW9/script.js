4355a7e4fff3ad77a8a1b6e677d61f6797b9d2a9
JulsLia <124190384+JulsLia@users.noreply.github.com>
2023-02-23 01:27:50 +0300

    make HW9

---
+++ HW9/script.js
@@ -0,0 +1,231 @@
+let classiсLasagne = {
+    name: 'Classic Lasagne',
+    nameingredients: 'Ingredients:',
+    ingredients: ['minced meat', 'bechamel sauce', 'lasagne', 'cheese'],
+    'name cost price': 'Cost price:',
+    'cost price': 200,
+    'name sale price': 'Sale price:',
+    'sale price': 450,
+    'profit': 'Net profit:',
+};
+alert('Classic Lasagne');
+alert('Ingredients:');
+alert(classiсLasagne.ingredients);
+alert('Cost price:');
+alert(classiсLasagne['cost price']);
+alert('Sale price:');
+alert(classiсLasagne['sale price']);
+alert('Net profit:');
+alert(classiсLasagne['sale price']-classiсLasagne['cost price']);
+
+
+
+
+
+let caesarSaladWithShrimp = {
+    name: 'Caesar Salad With Shrimp',
+    nameingredients: 'Ingredients:',
+    ingredients: ['romano salad', 'shrimp', 'cherry tomatoes', 'crackers', 'salad dressing', 'parmesan cheese'],
+    'name cost price': 'Cost price:',
+    'cost price': 220,
+    'name sale price': 'Sale price:',
+    'sale price': 480,
+    'profit': 'Net profit:',
+};
+alert('Caesar Salad With Shrimp');
+alert('Ingredients:');
+alert(caesarSaladWithShrimp.ingredients);
+alert('Cost price:');
+alert(caesarSaladWithShrimp['cost price']);
+alert('Sale price:');
+alert(caesarSaladWithShrimp['sale price']);
+alert('Net profit:');
+alert(caesarSaladWithShrimp['sale price']-caesarSaladWithShrimp['cost price']);
+
+
+
+
+let eggplantSalad = {
+    name: 'Eggplant Salad',
+    nameingredients: 'Ingredients:',
+    ingredients: ['fried eggplant', 'tomatoes', 'bulgarian pepper', 'red onion', 'mozzarella', 'parsley', 'dill', 'cilantro', 'corn starch'],
+    'name cost price': 'Cost price:',
+    'cost price': 100,
+    'name sale price': 'Sale price:',
+    'sale price': 300,
+    'profit': 'Net profit:',
+};
+alert('Eggplant Salad');
+alert('Ingredients:');
+alert(eggplantSalad.ingredients);
+alert('Cost price:');
+alert(eggplantSalad['cost price']);
+alert('Sale price:');
+alert(eggplantSalad['sale price']);
+alert('Net profit:');
+alert(eggplantSalad['sale price']-eggplantSalad['cost price']);
+
+
+
+
+let creamSoupOfChampignons = {
+    name: 'Cream Soup Of Champignons',
+    nameingredients: 'Ingredients:',
+    ingredients: ['champignons', 'potatoes', 'onion', 'cream'],
+    'name cost price': 'Cost price:',
+    'cost price': 150,
+    'name sale price': 'Sale price:',
+    'sale price': 350,
+    'profit': 'Net profit:',
+};
+alert('Cream Soup Of Champignons');
+alert('Ingredients:');
+alert(creamSoupOfChampignons.ingredients);
+alert('Cost price:');
+alert(creamSoupOfChampignons['cost price']);
+alert('Sale price:');
+alert(creamSoupOfChampignons['sale price']);
+alert('Net profit:');
+alert(creamSoupOfChampignons['sale price']-creamSoupOfChampignons['cost price']);
+
+
+
+
+
+let pizzaBarbecue = {
+    name: 'Pizza Barbecue',
+    nameingredients: 'Ingredients:',
+    ingredients: ['dough', 'barbecue sauce', 'chicken fillet', 'mozzarella', 'parmesan cheese', 'olives', 'red onion', 'tomato sauce'],
+    'name cost price': 'Cost price:',
+    'cost price': 170,
+    'name sale price': 'Sale price:',
+    'sale price': 600,
+    'profit': 'Net profit:',
+};
+alert('Pizza Barbecue');
+alert('Ingredients:');
+alert(pizzaBarbecue.ingredients);
+alert('Cost price:');
+alert(pizzaBarbecue['cost price']);
+alert('Sale price:');
+alert(pizzaBarbecue['sale price']);
+alert('Net profit:');
+alert(pizzaBarbecue['sale price']-pizzaBarbecue['cost price']);
+
+
+
+
+
+let pastaBolognese = {
+    name: 'Pasta Bolognese',
+    nameingredients: 'Ingredients:',
+    ingredients: ['ground beef', 'spaghetti', 'tomato paste', 'onion', 'parmesan cheese', 'basil', 'garlic' ],
+    'name cost price': 'Cost price:',
+    'cost price': 180,
+    'name sale price': 'Sale price:',
+    'sale price': 450,
+    'profit': 'Net profit:',
+};
+alert('Pasta Bolognese');
+alert('Ingredients:');
+alert(pastaBolognese.ingredients);
+alert('Cost price:');
+alert(pastaBolognese['cost price']);
+alert('Sale price:');
+alert(pastaBolognese['sale price']);
+alert('Net profit:');
+alert(pastaBolognese['sale price']-pastaBolognese['cost price']);
+
+
+
+
+let grilledBeefWithPotatoesAndEggplant = {
+    name: 'Grilled Beef With Potatoes And Eggplant',
+    nameingredients: 'Ingredients:',
+    ingredients: ['beef', 'potatoes', 'eggplant'],
+    'name cost price': 'Cost price:',
+    'cost price': 300,
+    'name sale price': 'Sale price:',
+    'sale price': 650,
+    'profit': 'Net profit:',
+};
+alert('Grilled Beef With Potatoes And Eggplant');
+alert('Ingredients:');
+alert(grilledBeefWithPotatoesAndEggplant.ingredients);
+alert('Cost price:');
+alert(grilledBeefWithPotatoesAndEggplant['cost price']);
+alert('Sale price:');
+alert(grilledBeefWithPotatoesAndEggplant['sale price']);
+alert('Net profit:');
+alert(grilledBeefWithPotatoesAndEggplant['sale price']-grilledBeefWithPotatoesAndEggplant['cost price']);
+
+
+
+
+
+let meringueRoll = {
+    name: 'Meringue Roll',
+    nameingredients: 'Ingredients:',
+    ingredients: ['sugar', 'egg whites', 'pistachios', 'pistachio paste', 'lemon juice', 'corn starch', 'raspberry', 'cream', 'mascarpone'],   
+    'name cost price': 'Cost price:',
+    'cost price': 100,
+    'name sale price': 'Sale price:',
+    'sale price': 300,
+    'profit': 'Net profit:',
+};
+alert('Meringue Roll');
+alert('Ingredients:');
+alert(meringueRoll.ingredients);
+alert('Cost price:');
+alert(meringueRoll['cost price']);
+alert('Sale price:');
+alert(meringueRoll['sale price']);
+alert('Net profit:');
+alert(meringueRoll['sale price']-meringueRoll['cost price']);
+
+
+
+
+let menu = {
+    name: 'Menu:',
+    menuname: ['Classis Lasagne', 'Caesar Salad With Shrimp', 'Eggplant Salad', 'Cream Soup Of Champignons', 'Pizza Barbecue', 'Pasta Bolognese', 'Grilled Beef With Potatoes And Eggplant', 'Meringue Roll'],
+};
+alert('Menu:');
+alert(menu.menuname);
+
+
+
+menu.menuname.pop();
+alert(menu.menuname);
+
+menu.menuname.unshift('Meringue Roll');
+alert(menu.menuname);
+
+meringueRoll.ingredients.shift();
+alert(meringueRoll.ingredients);
+
+menu.menuname.splice(0,7);
+alert(menu.menuname);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
