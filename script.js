d9fe60ed0873e17832a0594545a83474d47872f1
JulsLia <124190384+JulsLia@users.noreply.github.com>
2023-02-23 22:06:17 +0300

    branch

---
+++ script.js
@@ -0,0 +1,59 @@
+const NAME = "My name: Yulia Sysoeva";
+console.log(NAME);
+const PLACEOFBIRTH = "Place of birth: Moscow";
+console.log(PLACEOFBIRTH);
+const STATUS = "Status: Learner"
+console.log(STATUS);
+let yearOfBirth = "Year of birth:";
+console.log(yearOfBirth)
+let year = 1990; 
+console.log(year);
+let thisYear1 = "This year:";
+console.log(thisYear1);
+let thisYear = 2023; 
+console.log(thisYear);
+let myAge = `Now ${thisYear - year} year`
+console.log(myAge);
+let aStoryAboutMe = `Немного обо мне. 
+Я люблю путешествовать,читать книги, смотреть сериалы и фильмы на английском, готовить торты и выпечку. Люблю посещать выставки, музеи и созерцать искусство и красоту.
+Мои хобби:
+- психология
+- путешествия
+- медитация
+- торты и выпечка
+
+Мои новые увлечения:
+- йога
+- программирование
+- английский язык`
+console.log(aStoryAboutMe);
+
+let aboutWork = `Мой опыт работы:`
+console.log(aboutWork);
+let archivist = `- архивариус (6 месяцев)`
+console.log(archivist);
+let archivist1 = 0.6; 
+console.log(archivist1);
+let consultant = `- продавец-консультант (6 месяцев)`
+console.log(consultant);
+let consultant1 = 0.6;  
+console.log(consultant1);
+let pcOperator =`- оператор ПК (1 год)`
+console.log(pcOperator);
+let pcOperator1 = 1;
+console.log(pcOperator1);
+let engineer = `- инженер-техник (1 год)`
+console.log(engineer);
+let engineer1 = 1;
+console.log(engineer1);
+let manager = `- менеджер отдела продаж (4 года)`
+console.log(manager);
+let manager1 = 4;
+console.log(manager1);
+let workExperience = `Мой стаж чуть более 7 лет, а именно ${archivist1 + consultant1 + pcOperator1 + engineer1 + manager1} лет`
+console.log(workExperience);
+
+let connection = `Связаться со мной
+- telegram: @yuliasysoeva
+- email: Juli-2813@yandex.ru`
+console.log(connection);
